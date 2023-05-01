60dca5dcec7eacc4984755282cc22ec24b11d6b6
nakyung128 <lnk12888@naver.com>
2023-05-01 23:09:44 +0900

    ✨ 구글 계정으로 로그인 구현

--- MuslingProject/MuslingProject/LoginViewController.swift
+++ MuslingProject/MuslingProject/LoginViewController.swift
@@ -6,15 +6,33 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
     
-    // 카카오 로그인 선택했을 때
-    @IBAction func kakaoLogin(_ sender: UIButton) {
+    // 구글 계정으로 로그인 선택했을 때
+    @IBAction func ggLogin(_ sender: UIButton) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            guard error == nil else { return }
+            guard let signInResult = signInResult else { return }
+            
+            let user = signInResult.user
+            let email = user.profile?.email
+            let name = user.profile?.name
+            
+            // 프로필 완성 화면으로 이동
+            let vcName = self.storyboard?.instantiateViewController(withIdentifier: "ProfileVC")
+            vcName?.modalPresentationStyle = .fullScreen
+            vcName?.modalTransitionStyle = .crossDissolve
+            self.present(vcName!, animated: true, completion: nil)
+            
+            
+        }
     }
     
     // 일반 로그인 선택했을 때
     @IBAction func defaultLogin(_ sender: UIButton) {
+        // 회원 가입 화면으로 전환
         let vcName = self.storyboard?.instantiateViewController(withIdentifier: "SignUp")
         vcName?.modalPresentationStyle = .fullScreen
         vcName?.modalTransitionStyle = .crossDissolve
--- MuslingProject/MuslingProject/SceneDelegate.swift
+++ MuslingProject/MuslingProject/SceneDelegate.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else { return }
+        let _ = GIDSignIn.sharedInstance.handle(url)
+    }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
